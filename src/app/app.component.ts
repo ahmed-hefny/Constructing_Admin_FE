03dcfc7420628927302e7d9ebcff4bfee4e2b077
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-06-18 00:37:09 +0300

    basic commit for login api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Constructing_Admin_FE';
+  httpClient = inject(HttpClient);
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const payload =
+    {
+      "username": "hefny",
+      "password": "123456"
+    }
+    this.httpClient.post(`https://localhost:7274/api/User/login`, payload)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.error('Error during login:', error);
+        }
+      });
+  }
 }
