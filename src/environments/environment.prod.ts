034f4c29f69932ec86511585f348f3df03744b3b
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-08-10 19:24:59 +0300

    add exception for well-known directory and production command

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://admin.ca-admin.com/',
+    apiUrl: 'http://admin.ca-admin.com/api',
     appName: 'Constructing Admin FE',
     version: '1.0.0',
     enableLogging: false,
