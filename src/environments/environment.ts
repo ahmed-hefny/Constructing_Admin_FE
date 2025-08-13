1aec2858bd28b063acff23362425b51953348dbd
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-08-14 02:44:07 +0300

    ui Fixes for mobile view

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5112/api',
+  apiUrl: 'https://admin.ca-admin.com/api',
   appName: 'Constructing Admin FE',
   version: '1.0.0',
   enableLogging: true,
