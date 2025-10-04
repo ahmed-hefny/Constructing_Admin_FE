7b66327f0791929f09800fcf6bf03bb2022ab68a
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-10-04 18:02:48 +0300

    update app version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     apiUrl: 'https://admin.ca-admin.com/api',
     appName: 'Constructing Admin FE',
-    version: '1.4.0',
+    version: '1.5.0',
     enableLogging: false,
     enableDebugMode: false,
 
