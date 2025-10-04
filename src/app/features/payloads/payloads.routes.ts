31b9b29b7de25fc9c7daded9e6c90b9de8ef4693
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-10-04 17:50:56 +0300

    allow supervisor to upload payload

--- src/app/features/payloads/payloads.routes.ts
+++ src/app/features/payloads/payloads.routes.ts
@@ -13,10 +13,6 @@ export default [
     },
     {
         path: ':projectId/:companyId/upload',
-        canActivate: [hasRoleGuard],
         loadComponent: () => import('./upload-payload/upload-payload.component').then(m => m.UploadPayloadComponent),
-        data: {
-            roles: [SystemRoles.ADMIN, SystemRoles.EMPLOYEE]
-        }
     }
 ];
