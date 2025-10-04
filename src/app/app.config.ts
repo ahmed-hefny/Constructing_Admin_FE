dcdd665c75db1c0da0fde1d5ddcf406cd5cd68aa
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-10-04 18:10:06 +0300

    scroll to top after each navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
@@ -16,7 +16,7 @@ export const appConfig: ApplicationConfig = {
     ConfirmationService,
     PrimeNGConfig,
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideHttpClient(
       withFetch(),
       withInterceptors([
