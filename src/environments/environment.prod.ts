0d534f226886ee8ad7be77744a7b46ef5ee945d7
ahmed hefny <ahmed.hefny@cmeoffshore.com>
2025-10-05 15:16:37 +0300

    allow camera when choose file from android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: true,
     apiUrl: 'https://admin.ca-admin.com/api',
     appName: 'Constructing Admin FE',
-    version: '1.5.0',
+    version: '1.5.1',
     enableLogging: false,
     enableDebugMode: false,
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'https://admin.ca-admin.com/api',
   appName: 'Constructing Admin FE',
-  version: '1.5.0',
+  version: '1.5.1',
   enableLogging: true,
   enableDebugMode: true,
 };
